f22c690b86f3d659333f8fc9908101b39406117e
Cyril Blaecke <cyril@dilolabs.fr>
2024-06-11 21:55:01 +0000

    UI - POC v2024.06.11

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,7 +1,11 @@
 class Chat < ApplicationRecord
   has_many :messages, dependent: :destroy
 
+  def first_question
+    messages.where(role: "user").order(:created_at).first&.content
+  end
+
   def last_question
-    messages.where(role: "user").order(:created_at).last.content
+    messages.where(role: "user").order(:created_at).last&.content
   end
 end
