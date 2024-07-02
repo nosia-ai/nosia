1c398d75f7414987ba2899871de4636f3f96ea4c
Cyril Blaecke <cyril@dilolabs.fr>
2024-07-02 23:07:23 +0200

    RAG with evaluation ✨ (#40)

--- config/initializers/langchainrb_rails.rb
+++ config/initializers/langchainrb_rails.rb
@@ -7,11 +7,12 @@ Faraday.default_connection_options = Faraday::ConnectionOptions.new({
 LangchainrbRails.configure do |config|
   config.vectorsearch = Langchain::Vectorsearch::Pgvector.new(
     llm: Langchain::LLM::Ollama.new(
-      url: ENV["OLLAMA_URL"] || "http://localhost:11434",
+      url: ENV.fetch("OLLAMA_URL", "http://localhost:11434"),
       default_options: {
-        completion_model_name: ENV["OLLAMA_COMPLETION_MODEL"] || "phi3:medium",
-        embeddings_model_name: ENV["OLLAMA_EMBEDDINGS_MODEL"] || "nomic-embed-text",
-        chat_completion_model_name: ENV["OLLAMA_CHAT_COMPLETION_MODEL"] || "phi3:medium",
+        chat_completion_model_name: ENV.fetch("OLLAMA_CHAT_COMPLETION_MODEL", "phi3:medium"),
+        completion_model_name: ENV.fetch("OLLAMA_COMPLETION_MODEL", "phi3:medium"),
+        embeddings_model_name: ENV.fetch("OLLAMA_EMBEDDINGS_MODEL", "nomic-embed-text"),
+        temperature: 0.1,
       }
     )
   )
