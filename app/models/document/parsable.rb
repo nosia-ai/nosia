e1af1adfb1963816c2c226e958aaeef7dce57a54
Cyril Blaecke <1692273+cbldev@users.noreply.github.com>
2024-11-18 20:28:09 +0100

    Support Markdown and text documents ✨ (#55)

--- app/models/document/parsable.rb
+++ app/models/document/parsable.rb
@@ -2,14 +2,30 @@ module Document::Parsable
   extend ActiveSupport::Concern
 
   def parse
-    self.file.blob.open do |io|
-      reader = ::PDF::Reader.new(io)
-      self.content = reader.pages.map(&:text).join("\n\n")
+    content_type = self.file.blob.content_type
+
+    if content_type.start_with?("text/")
+      parse_text
+    elsif content_type.eql?("application/pdf")
+      parse_pdf
     end
   end
 
   def parse!
     parse
     save!
+  end
+
+  def parse_pdf
+    self.file.blob.open do |io|
+      reader = ::PDF::Reader.new(io)
+      self.content = reader.pages.map(&:text).join("\n\n")
+    end
+  end
+
+  def parse_text
+    self.file.blob.open do |io|
+      self.content = io.read
+    end
   end
 end
