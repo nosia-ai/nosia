f9ef0088c2d2223df97228b0a3bb3cdbf63500b6
Cyril Blaecke <1692273+cbldev@users.noreply.github.com>
2024-11-19 15:45:27 +0000

    Fix chunkify 🐛

--- app/models/document/chunkable.rb
+++ app/models/document/chunkable.rb
@@ -6,10 +6,12 @@ module Document::Chunkable
   end
 
   def chunkify!
+    separators = JSON.parse(ENV.fetch("SEPARATORS", [ "\n\n", "\n", " " ]))
+
     splitter = ::Baran::RecursiveCharacterTextSplitter.new(
-      chunk_size: ENV.fetch("CHUNK_SIZE", 1000),
-      chunk_overlap: ENV.fetch("CHUNK_SIZE", 200),
-      separators: ENV.fetch("SEPARATORS", [ "\n\n", "\n", " " ])
+      chunk_size: ENV.fetch("CHUNK_SIZE", 1000).to_i,
+      chunk_overlap: ENV.fetch("CHUNK_OVERLAP", 200).to_i,
+      separators:,
     )
 
     new_chunks = splitter.chunks(self.content)
