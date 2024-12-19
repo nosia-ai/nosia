139b94dc21511f0b386180fa77108e0b4879ccdc
Cyril Blaecke <1692273+cbldev@users.noreply.github.com>
2024-12-19 21:53:52 +0000

    Prepare Infomaniak compatibility and multi-accounts

--- app/models/chat/completionable.rb
+++ app/models/chat/completionable.rb
@@ -8,21 +8,37 @@ module Chat::Completionable
   end
 
   def complete(model: nil, temperature: nil, top_k: nil, top_p: nil, max_tokens: nil, &block)
-    max_tokens ||= ENV.fetch("LLM_MAX_TOKENS", 1_024).to_i
-    model ||= ENV.fetch("LLM_MODEL", "mixtral")
-    temperature ||= ENV.fetch("LLM_TEMPERATURE", 0.1).to_f
-    top_k ||= ENV.fetch("LLM_TOP_K", 40).to_f
-    top_p ||= ENV.fetch("LLM_TOP_P", 0.9).to_f
+    options = default_options.merge(
+      {
+        model:,
+        temperature:,
+        top_k:,
+        top_p:,
+        max_tokens:
+      }.compact_blank
+    )
 
-    case Chat.ai_provider
+    case self.class.ai_provider
     when "ollama"
-      complete_with_ollama(top_k:, top_p:, &block)
+      complete_with_ollama(top_k: options[:top_k], top_p: options[:top_p], &block)
     when "infomaniak"
-      complete_with_infomaniak(model:, temperature:, top_p:, max_tokens:, &block)
+      complete_with_infomaniak(model: options[:model], temperature: options[:temperature], top_p: options[:top_p], max_tokens: options[:max_tokens], &block)
+    else
+      raise "Unsupported AI provider: #{self.class.ai_provider}"
     end
   end
 
   private
+
+  def default_options
+    {
+      max_tokens: ENV.fetch("LLM_MAX_TOKENS", 1_024).to_i,
+      model: ENV.fetch("LLM_MODEL", "mixtral"),
+      temperature: ENV.fetch("LLM_TEMPERATURE", 0.1).to_f,
+      top_k: ENV.fetch("LLM_TOP_K", 40).to_f,
+      top_p: ENV.fetch("LLM_TOP_P", 0.9).to_f
+    }
+  end
 
   def retrieval_fetch_k
     ENV.fetch("RETRIEVAL_FETCH_K", 4)
--- app/models/chat/infomaniak.rb
+++ app/models/chat/infomaniak.rb
@@ -23,36 +23,40 @@ module Chat::Infomaniak
   def complete_with_infomaniak(model:, temperature:, top_p:, max_tokens:, &block)
     question = last_question
 
-    context = []
-
-    messages_for_assistant = []
-    messages_for_assistant << { role: "system", content: system_prompt }
-    messages_for_assistant << messages_hash if messages_hash.any?
+    messages_for_assistant = prepare_messages(question)
 
     assistant_response = messages.create(role: "assistant", done: false, content: "", response_number:)
     assistant_response.broadcast_created
 
-    retrieved_chunks = Chunk.where(account:).similarity_search(question, k: retrieval_fetch_k)
-    assistant_response.update(similar_document_ids: retrieved_chunks.pluck(:chunkable_id).uniq) if retrieved_chunks.any?
+    context = fetch_context(question)
+    prompt = generate_prompt(context, question) if context.present?
 
-    if retrieved_chunks.any?
-      context << retrieved_chunks.map(&:content).join("\n\n")
-      context = context.join("\n\n")
-
-      prompt = ENV.fetch("QUERY_PROMPT_TEMPLATE", "Nosia helpful content: {context}\n\n---\n\nNow here is the question you need to answer.\n\nQuestion: {question}")
-      prompt = prompt.gsub("{context}", context)
-      prompt = prompt.gsub("{question}", question)
-
-      messages_for_assistant.pop
-      messages_for_assistant << { role: "user", content: prompt }
-    end
-
-    messages_for_assistant = messages_for_assistant.flatten
+    messages_for_assistant[-1][:content] = prompt if prompt
 
     llm = Chat.new_infomaniak_llm(model:)
     llm_response = llm.chat(messages: messages_for_assistant, temperature:, top_p:, max_tokens:, &block)
 
     assistant_response.update(done: true, content: llm_response.completion)
     assistant_response
+  end
+
+  private
+
+  def prepare_messages(question)
+    messages = [ { role: "system", content: system_prompt } ]
+    messages << messages_hash if messages_hash.any?
+    messages.flatten
+  end
+
+  def fetch_context(question)
+    retrieved_chunks = Chunk.where(account:).similarity_search(question, k: retrieval_fetch_k)
+    return unless retrieved_chunks.any?
+
+    retrieved_chunks.map(&:content).join("\n\n")
+  end
+
+  def generate_prompt(context, question)
+    prompt_template = ENV.fetch("QUERY_PROMPT_TEMPLATE", "Nosia helpful content: {context}\n\n---\n\nNow here is the question you need to answer.\n\nQuestion: {question}")
+    prompt_template.gsub("{context}", context).gsub("{question}", question)
   end
 end
