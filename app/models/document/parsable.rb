867c30b9fd099d3823cfe7b121e6e10301446ca7
Cyril Blaecke <1692273+cbldev@users.noreply.github.com>
2024-12-21 21:25:01 +0000

    Prepare Docling

--- app/models/document/parsable.rb
+++ app/models/document/parsable.rb
@@ -6,6 +6,8 @@ module Document::Parsable
 
     if content_type.start_with?("text/")
       parse_text
+    elsif ENV["DOCLING_SERVE_BASE_URL"].present?
+      parse_with_docling
     elsif content_type.eql?("application/pdf")
       parse_pdf
     end
@@ -27,5 +29,35 @@ module Document::Parsable
     self.file.blob.open do |io|
       self.content = io.read
     end
+  end
+
+  def parse_with_docling
+    connection = Faraday.new(url: ENV["DOCLING_SERVE_BASE_URL"])
+
+    base64_string = Base64.strict_encode64(self.file.download)
+
+    request_body = {
+      file_source: {
+        base64_string:,
+        filename: self.file.filename.to_s
+      },
+      options: {
+        include_images: false,
+        output_docling_document: false,
+        output_markdown: true
+      }
+    }
+
+    response = connection.post do |request|
+      request.url "/convert"
+      request.headers["Content-Type"] = "application/json"
+      request.headers["Accept"] = "application/json"
+      request.body = request_body.to_json
+    end
+
+    return unless response.success?
+    parsed_response = JSON.parse(response.body)
+    self.content = parsed_response.dig("document", "markdown")
+    self.save!
   end
 end
--- app/models/website/crawlable.rb
+++ app/models/website/crawlable.rb
@@ -2,8 +2,32 @@ module Website::Crawlable
   extend ActiveSupport::Concern
 
   def crawl!
-    # TODO: Crawl URL content and save it in data
-    self.data = url
-    save!
+    return unless ENV["DOCLING_SERVE_BASE_URL"].present?
+
+    connection = Faraday.new(url: ENV["DOCLING_SERVE_BASE_URL"])
+
+    request_body = {
+      http_source: {
+        url: self.url
+      },
+      options: {
+        include_images: false,
+        output_docling_document: false,
+        output_markdown: true
+      }
+    }
+
+    response = connection.post do |request|
+      request.url "/convert"
+      request.headers["Content-Type"] = "application/json"
+      request.headers["Accept"] = "application/json"
+      request.body = request_body.to_json
+    end
+
+    Rails.logger.info response
+    return unless response.success?
+    parsed_response = JSON.parse(response.body)
+    self.data = parsed_response.dig("document", "markdown")
+    self.save!
   end
 end
